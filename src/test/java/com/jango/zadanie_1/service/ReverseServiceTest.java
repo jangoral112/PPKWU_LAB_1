bde8912aa988de52d461cbae31590588b698169e
Jan Góral <jan.goral@epassi.com>
2021-10-14 13:14:14 +0200

    Added shouldReturnReversedString test - autowire does not work

--- src/test/java/com/jango/zadanie_1/service/ReverseServiceTest.java
+++ src/test/java/com/jango/zadanie_1/service/ReverseServiceTest.java
@@ -1,5 +1,27 @@
 package com.jango.zadanie_1.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ReverseServiceTest {
+
+    @Autowired
+    private ReverseService reverseService;
+
+    @Test
+    public void shouldReturnReversedString() {
+        // Given
+        String stringToReverse = "a123456789C0";
+
+        // When
+        String result = reverseService.reverseString(stringToReverse);
+
+        // Then
+        String expectedString = "0C987654321a";
+
+        assertEquals(expectedString, result);
+    }
 
 }
